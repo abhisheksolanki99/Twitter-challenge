d24122ad95320a81780d174c75460d0c65056e6b
Abhishek Solanki <solankiabhi90@gmail.com>
2016-09-21 19:51:42 +0530

    Create config.php

---
+++ api/config.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Your Twitter App Info
+ */
+// Consumer Key
+define('CONSUMER_KEY', 'cvh6DdMa7XUavOm3fbrEmXq9S');
+define('CONSUMER_SECRET', 'HTMuEo8WSi5RW9fFnaJ4fhqEi7vTUi9SFMqhG44zCzgWJHSDO3');
+
+// User Access Token
+define('ACCESS_TOKEN', '2221810244-aAlgwmRIskDICXz19XRgvwdz9xvMZ9sH3zmsGZl');
+define('ACCESS_SECRET', 'ZcAQ1WHTjgFTEkqiUcmBA7XA9oSlrp624lG6Hohv08vsV');
+
+// Cache Settings
+define('CACHE_ENABLED', false);
+define('CACHE_LIFETIME', 3600); // in seconds
+define('HASH_SALT', md5(dirname(__FILE__)));
